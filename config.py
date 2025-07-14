ea6d0a01a5a380d02bfbec578d70eedf212e0533
Dev-Dexar <dlovesfact@gmail.com>
2025-07-14 23:14:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("7597274168:AAEpMjinTuta78O2YmGc1YJSv5aFxZO7GzE")
 RAZORPAY_KEY = os.getenv("rzp_live_Kfvz8iobE8iUZc")
 RAZORPAY_SECRET = os.getenv("bcPhJQ2pHTaaF94FhWCEl6eD")
 CHANNEL_ID = int(os.getenv("-1002755139288"))
-ADMIN_ID = int(os.getenv("-7209883949"))
+ADMIN_ID = int(os.getenv("7209883949"))
 MONGO_URI = os.getenv("mongodb+srv://Sinchu:Sinchu@sinchu.qwijj.mongodb.net/?retryWrites=true&w=majority&appName=Sinchu")
 ADMIN_SECRET = os.getenv("ADMIN_SECRET", "12345")
 
