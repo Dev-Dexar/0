17d05a7a2bf950aba8f9f025ab27b837c2417b95
Dev-Dexar <dlovesfact@gmail.com>
2025-07-14 23:08:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-RAZORPAY_KEY = os.getenv("RAZORPAY_KEY")
-RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-MONGO_URI = os.getenv("MONGO_URI")
+BOT_TOKEN = os.getenv("7597274168:AAEpMjinTuta78O2YmGc1YJSv5aFxZO7GzE")
+RAZORPAY_KEY = os.getenv("rzp_live_Kfvz8iobE8iUZc")
+RAZORPAY_SECRET = os.getenv("bcPhJQ2pHTaaF94FhWCEl6eD")
+CHANNEL_ID = int(os.getenv("-1002755139288"))
+ADMIN_ID = int(os.getenv("7209883949"))
+MONGO_URI = os.getenv("mongodb+srv://Sinchu:Sinchu@sinchu.qwijj.mongodb.net/?retryWrites=true&w=majority&appName=Sinchu")
 ADMIN_SECRET = os.getenv("ADMIN_SECRET", "12345")
 
 RAZORPAY_PLANS = {
